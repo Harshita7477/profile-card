507f8197a8a278f05299adafc1ccfddad073163a
Harshita7477 <harshitagupta7477@gmail.com>
2026-07-19 12:12:41 +0530

    Update script

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ btn.classList.toggle("following");
 msg.addEventListener("click", function(){
  alert("Message sent to Rosy! 📩");
 });
+
+
+/*  Ab ek aur important cheez
+
+Agar kal tum style.css me color change karti ho.
+
+To GitHub automatically update nahi hoga.
+
+Har baar ye 3 commands chalani hongi:
+
+git add .
+git commit -m "Change button color"
+git push
+
+Bas.
+
+Ab git remote add origin dobara kabhi nahi karna padega is project me, kyunki ek baar connection ban chuka hai. */
